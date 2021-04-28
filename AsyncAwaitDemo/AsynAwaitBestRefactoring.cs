5fd3fe9baaa01f938401a3ff40b85b3fba3449ca
Nouman Baloch <noumanbaloch@Noumans-MacBook-Air.local>
2021-04-28 21:39:14 +0500

    Initial Commit

---
+++ AsyncAwaitDemo/AsynAwaitBestRefactoring.cs
@@ -0,0 +1,120 @@
+﻿using AsyncAwaitBestPractices;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitDemo
+{
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+
+    public class AsynAwaitBestRefactoring
+    {
+        //Refactoring Constructor
+        public AsynAwaitBestRefactoring()
+        {
+            ExecuteInitialization().SafeFireAndForget();
+        }
+
+        async Task ExecuteInitialization()
+        {
+            await Task.Delay(3000);
+        }
+        async Task FirstRefactoring()
+        {
+            //First Refactoring
+            //IsValid(true).Wait();
+
+            #region After Refactoring
+            //just user await keyword
+            await IsValid(true);
+            //If you really want to wait then use following
+            IsValid(true).GetAwaiter().GetResult();
+            #endregion
+
+        }
+
+        async Task SecondRefactoring()
+        {
+            //Second Refactoring
+            //List<int> Identifiers = await GetAllIds(); //thread 10
+
+            //foreach(var id in Identifiers)
+            //{
+            //    await GetDetailsById(id); // thread 20
+            //}
+
+            #region After Refactoring
+
+            List<int> Identifiers = await GetAllIds().ConfigureAwait(false); //thread 20
+
+            foreach (var id in Identifiers)
+            {
+                await GetDetailsById(id).ConfigureAwait(false);
+            }
+            #endregion
+        }
+
+        Task<byte[]> ThirdRefactoring()
+        {
+            //Third Refactoring
+            //WebClient wc = new WebClient();
+            //return wc.DownloadDataTaskAsync("https://www.slideshare.net/NoumanBaloch5/dining-philosopher-problem-124674804");
+
+            #region After Refactoring
+            WebClient wc = new WebClient();
+            return wc.DownloadDataTaskAsync("https://www.slideshare.net/noumanbaloch5/dining-philosopher-problem-124674804");
+
+            #endregion
+        }
+
+        async Task<byte[]> FourthRefactoring()
+        {
+            try
+            {
+                WebClient wc = new WebClient();
+                return await wc.DownloadDataTaskAsync("https://www.slideshare.net/NoumanBaloch5/dining-philosopher-problem-124674804");
+
+            }
+            catch (Exception)
+            {
+
+                throw new Exception();
+            }
+            
+        }
+
+        async Task<Person> GetDetailsById(int Id)
+        {
+            List<Person> person = new List<Person>
+            {
+                new Person { ID = 1, Name = "Steve", Age = 24},
+                new Person { ID = 3, Name = "John", Age = 20},
+                new Person { ID = 3, Name = "David", Age = 30}
+
+            };
+            await Task.Delay(3000);
+            return person.Find(x => x.ID == Id);
+        }
+        private async Task<List<int>> GetAllIds()
+        {
+            List<int> Ids = new List<int> { 1, 2, 3 };
+            await Task.Delay(3000);
+            return Ids;
+        }
+
+        private async Task<bool> IsValid(bool input)
+        {
+            await Task.Delay(2000);
+            return true;
+        }
+    }
+
+   
+}
---
+++ AsyncAwaitDemo/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncAwaitDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "InProgress!";
+            string url = ("https://www.slideshare.net/NoumanBaloch5/dining-philosopher-problem-124674804");
+            //ReadDataFromUrl(url);
+            label2.Text = "Completed";
+        }
+
+        private async Task<string> ReadDataFromUrl(string url)
+        {
+            WebClient wc = new WebClient();
+            byte[] result = await wc.DownloadDataTaskAsync(url); //thread 5
+            string data = Encoding.ASCII.GetString(result);
+            return data;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            label2.Text = "InProgress!";
+            string url = ("https://www.slideshare.net/NoumanBaloch5/dining-philosopher-problem-124674804");
+            await ReadDataFromUrl(url); 
+            label2.Text = "Completed";
+        }
+    }
+}
